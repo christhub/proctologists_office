a1b038d8af4ffd8a2d4b52fb242efae9ebaa3215
Ryan Heupler & Clayton Easterbrooks <student@epicodus.com>
2015-08-11 10:03:55 -0700

    integration spec passing allows sinatra to run and submit tasks successfully

---
+++ app.rb
@@ -0,0 +1,16 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/to_do')
+
+get('/') do
+  @tasks = Task.view()
+  erb(:index)
+end
+
+post('/to_do') do
+  description = params.fetch('description')
+  task = Task.new(description)
+  task.save()
+  erb(:to_do)
+end
