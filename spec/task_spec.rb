41e80e38375e57b33149b8b2db97f119d5fe9797
Clayton Easterbrooks & Christian Lei <student@epicodus.com>
2015-08-17 13:45:02 -0700

    passs list specs, spec helper file added for cleanliness

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,22 +1,24 @@
-require('rspec')
-require('pg')
-require('task')
-require('pry')
+# require('rspec')
+# require('pg')
+# require('task')
+# require('pry')
+# require('list')
+require('spec_helper.rb')
 
-DB = PG.connect({:dbname => 'to_do_test'})
-
-RSpec.configure do |config|
-  config.after(:each) do
-    DB.exec("DELETE FROM tasks *;")
-  end
-end
+# DB = PG.connect({:dbname => 'to_do_test'})
+#
+# RSpec.configure do |config|
+#   config.after(:each) do
+#     DB.exec("DELETE FROM tasks *;")
+#   end
+# end
 
 describe(Task) do
 
   describe('#==') do
     it('is the same task if it has the same description') do
-      task1 = Task.new({:description => "learn SQL"})
-      task2 = Task.new({:description => "learn SQL"})
+      task1 = Task.new({:description => "learn SQL", :list_id => 1})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1})
       expect(task1).to(eq(task2))
     end
   end
@@ -29,9 +31,23 @@ describe(Task) do
 
   describe("#save") do
     it("adds a task to the array of saved tasks") do
-      test_task = Task.new({:description => "learn SQL"})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
       test_task.save()
       expect(Task.all()).to(eq([test_task]))
+    end
+  end
+
+  describe('#description') do
+    it('reads description out of database') do
+      test_task = Task.new({:description => 'learn something', :list_id => 1})
+      expect(test_task.description()).to(eq('learn something'))
+    end
+  end
+
+  describe('#list_id') do
+    it('lets you read out list ID') do
+      test_task = Task.new({:description => 'learn something', :list_id => 1})
+      expect(test_task.list_id()).to(eq(1))
     end
   end
 end
